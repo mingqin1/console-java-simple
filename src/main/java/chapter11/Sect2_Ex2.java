dc77868d2868a6342e4c38ffc4f6d599cedd07c5
simivalley <Ming@LAPTOP-VENUS>
2019-04-19 21:50:06 -0700

    add chapter 11 two java programs

---
+++ src/main/java/chapter11/Sect2_Ex2.java
@@ -0,0 +1,11 @@
+package chapter11;
+
+import java.util.Optional;
+public class Sect2_Ex2 {
+    public static void main(String[] args)
+    {
+        Optional<Integer> imNull = Optional.ofNullable(null);
+        if (imNull.isEmpty())
+            System.out.println("Empty");
+    }
+}
